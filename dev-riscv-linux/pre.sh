6a060057d8196b655c684bdf03a32c601099792d
Lehua Ding <lehua.ding@rivai.ai>
2023-12-05 10:19:49 +0800

    add dependencies

--- dev-riscv-linux/pre.sh
+++ dev-riscv-linux/pre.sh
@@ -2,6 +2,7 @@
 
 set -ex
 
+rm -rf home
 mkdir -p home/apps/src
 
 wget https://download.qemu.org/qemu-8.1.3.tar.xz
@@ -9,14 +10,11 @@ wget https://gitlab.com/qemu-project/libslirp/-/archive/v4.7.0/libslirp-v4.7.0.t
 tar -xvf qemu-8.1.3.tar.xz -C home/apps/src
 tar -xvf libslirp-v4.7.0.tar.gz -C home/apps/src
 
-wget https://github.com/ohmyzsh/ohmyzsh/archive/refs/heads/master.zip -O ohmyzsh-master.zip
-unzip ohmyzsh-master.zip
+unzip ../common/ohmyzsh-master.zip
 mv ohmyzsh-master home/.oh-my-zsh
 
-wget https://github.com/zsh-users/zsh-syntax-highlighting/archive/refs/heads/master.zip -O zsh-syntax-highlighting-master.zip
-unzip zsh-syntax-highlighting-master.zip
+unzip ../common/zsh-syntax-highlighting-master.zip
 mv zsh-syntax-highlighting-master home/zsh-syntax-highlighting
 
-wget https://github.com/zsh-users/zsh-autosuggestions/archive/refs/heads/master.zip -O zsh-autosuggestions-master.zip
-unzip zsh-autosuggestions-master.zip
+unzip ../common/zsh-autosuggestions-master.zip
 mv zsh-autosuggestions-master home/zsh-autosuggestions
--- dev/pre.sh
+++ dev/pre.sh
@@ -2,16 +2,14 @@
 
 set -ex
 
-mkdir -p home/apps/src
+rm -rf home
+mkdir -p home
 
-wget https://github.com/ohmyzsh/ohmyzsh/archive/refs/heads/master.zip -O ohmyzsh-master.zip
-unzip ohmyzsh-master.zip
+unzip ../common/ohmyzsh-master.zip
 mv ohmyzsh-master home/.oh-my-zsh
 
-wget https://github.com/zsh-users/zsh-syntax-highlighting/archive/refs/heads/master.zip -O zsh-syntax-highlighting-master.zip
-unzip zsh-syntax-highlighting-master.zip
+unzip ../common/zsh-syntax-highlighting-master.zip
 mv zsh-syntax-highlighting-master home/zsh-syntax-highlighting
 
-wget https://github.com/zsh-users/zsh-autosuggestions/archive/refs/heads/master.zip -O zsh-autosuggestions-master.zip
-unzip zsh-autosuggestions-master.zip
+unzip ../common/zsh-autosuggestions-master.zip
 mv zsh-autosuggestions-master home/zsh-autosuggestions
